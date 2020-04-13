a48ecdc15934d28e9a6daf577136736c63e297f8
Seulmin Ryu <sr4632@nyu.edu>
2020-04-13 14:20:33 -0400

    Initial Commit

---
+++ Chop-Chop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chop-Chop
+//
+//  Created by Seulmin Andy Ryu on 4/13/20.
+//  Copyright © 2020 Seulmin Ryu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
