2e27ff535c4729572d5bcfe40a7d55e812c3897d
Seulmin Ryu <sr4632@nyu.edu>
2020-04-15 02:55:51 -0400

    added google sign in button

--- Chop-Chop/ViewController.swift
+++ Chop-Chop/ViewController.swift
@@ -7,14 +7,29 @@
 //
 
 import UIKit
+import GoogleSignIn
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        GIDSignIn.sharedInstance()?.presentingViewController = self
+        // Automatically sign in the user.
+        GIDSignIn.sharedInstance()?.restorePreviousSignIn()
+        
+        let gSignIn = GIDSignInButton(frame: CGRect(x: 0, y: 0, width: 230, height: 48))
+        gSignIn.center = view.center
+        view.addSubview(gSignIn)
+        
         // Do any additional setup after loading the view.
     }
-
-
+    
+    //Sign out function we can use later on
+    //Do something like UIButton.addTarget(self, action: #selector(self.signOut(_:)), for: .touchUpInside)
+    //Referenced https://www.youtube.com/watch?v=BjsJNpgsl5c
+    @objc func signOut(_ sender: UIButton) {
+        print("Signing Out")
+        GIDSignIn.sharedInstance().signOut()
+    }
 }
 
